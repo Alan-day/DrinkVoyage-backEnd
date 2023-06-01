a9f3ecc3bd858c9d31111c504b08c321cba1ebb0
Alan-day <lesterthenovice@gmail.com>
2023-06-01 16:07:21 +0100

    boilerplate code

---
+++ src/main/java/com/nology/drinkvoyage/DrinkVoyageApplication.java
@@ -0,0 +1,13 @@
+package com.nology.drinkvoyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkVoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkVoyageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nology/drinkvoyage/DrinkVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nology.drinkvoyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkVoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
